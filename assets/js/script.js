a4ed762a3d92bbe9c03298768af951806506be25
luisgcodes <lcienfuegos95@gmail.com>
2022-03-13 19:53:17 -0700

    refactoring the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,38 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  // creates list item
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
+   // packages data as an object
+   var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
 
-  //  div to hold task info and adds to list item
-  var taskInfoEl = document.createElement("div");
-  taskInfoEl.className = "task-info";
+  // send as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+}
 
-  // adds HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-  listItemEl.appendChild(taskInfoEl);
+var createTaskEl = function(taskDataObj) {
+ // creates list item
+ var listItemEl = document.createElement("li");
+ listItemEl.className = "task-item";
 
-  // adds entire list item to list
-  tasksToDoEl.appendChild(listItemEl);
-};
+ //  div to hold task info and adds to list item
+ var taskInfoEl = document.createElement("div");
+ taskInfoEl.className = "task-info";
 
-formEl.addEventListener("submit", createTaskHandler);
+ // adds HTML content to div
+ taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+ listItemEl.appendChild(taskInfoEl);
+
+ // adds entire list item to list
+ tasksToDoEl.appendChild(listItemEl);
+}
+
+
+
+formEl.addEventListener("submit", taskFormHandler);
